64df8e40b1d4e5fc7f00040f505f5ff398195edc
Mateus Reis <mateuskonige@outlook.com>
2021-12-13 10:55:30 -0300

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(Auth::user(), 200);
+        }
+    }
+
+    public function logout() {
+        Auth::logout();
+    }
+}
